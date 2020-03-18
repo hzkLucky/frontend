ce8409467771dbc4f9ed3f49104449cd17e2fcf0
hzkLucky <47072290+hzkLucky@users.noreply.github.com>
2020-03-18 14:39:04 +0800

    Update NavMenu.vue

--- src/components/ui/NavMenu.vue
+++ src/components/ui/NavMenu.vue
@@ -3,7 +3,7 @@
    <div class="info">
        <div class="info__logo">
            <img src="../../assets/logo.png" alt="">
-           <span class="info__name">Raysiu</span>
+           <span class="info__name">Rays</span>
        </div>
    </div>
    <div class="menu-content">
